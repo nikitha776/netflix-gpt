f08ea029462fc6d886729d9fd3aee45fe8393ce5
nikitha776 <vlnikitha2006@gmail.com>
2024-11-16 23:38:37 +0530

    Built Secondary Container

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,3 +7,5 @@ export const API_OPTIONS = {
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MDFmN2I4YjdmZjZjMWRkNTg4ZjgzZWE4MjY2YjA0NSIsIm5iZiI6MTczMTc1MjY1MC4xOTY4OTA4LCJzdWIiOiI2NzM4NzAyYTM1ZjFlNmQxN2QyZTlkMmUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.G5LVhRx-IeerqcLQGjS6Qvy8DXlB7PN_AZcGW7eFblY'
   }
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -4,6 +4,9 @@ const movieSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlayingMovies: null,
+    popularMovies : null,
+    topRatedMovies : null,
+    upcomingMovies : null,
     trailerVideo: null
   },
   reducers: {
@@ -12,9 +15,18 @@ const movieSlice = createSlice({
     },
     addTrailerVideo: (state, action) => {
       state.trailerVideo = action.payload;
+    },
+    addPopularMovies: (state, action) => {
+      state.popularMovies = action.payload;
+    },
+    addTopRatedMovies: (state, action) => {
+      state.topRatedMovies = action.payload;
+    },
+    addUpcomingMovies: (state, action) => {
+      state.upcomingMovies = action.payload;
     }
   }
 });
 
-export const { addNowPlayingMovies, addTrailerVideo } = movieSlice.actions;
+export const { addNowPlayingMovies, addTrailerVideo , addPopularMovies , addTopRatedMovies , addUpcomingMovies } = movieSlice.actions;
 export default movieSlice.reducer;
